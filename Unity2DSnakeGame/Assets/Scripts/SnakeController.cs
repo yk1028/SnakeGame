97f1fb07d9b941222d3270b25c2983f18cbd4ef7
songwongeun <wongeunsong94@gmail.com>
2022-01-18 14:14:42 +0900

    feat: Snake 구현 - 머리 이동 방향으로 회전

--- Unity2DSnakeGame/Assets/Scripts/SnakeController.cs
+++ Unity2DSnakeGame/Assets/Scripts/SnakeController.cs
@@ -9,23 +9,21 @@ public class SnakeController : MonoBehaviour
     public GameObject headPrefab;
 
     private GameObject head;
-    private Vector2 nextDir;
+    private Vector2 headDirection;
 
     void Start()
     {
         head = Instantiate(headPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        nextDir = new Vector2(1, 0);
+        headDirection = new Vector2(1, 0);
     }
 
     void Update()
     {
-        CheckTouchForDirection();
+        ChangeOfHeadDirectionByTouch();
         UpdateHead();
-
-        
     }
-
-    private void CheckTouchForDirection()
+    
+    private void ChangeOfHeadDirectionByTouch()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -38,15 +36,25 @@ public class SnakeController : MonoBehaviour
 
     private void UpdateHead()
     {
-        head.transform.position = new Vector2(head.transform.position.x + nextDir.x * SPEED * Time.deltaTime,
-            head.transform.position.y + nextDir.y * SPEED * Time.deltaTime);
+        head.transform.position = new Vector2(head.transform.position.x + headDirection.x * SPEED * Time.deltaTime,
+            head.transform.position.y + headDirection.y * SPEED * Time.deltaTime);
     }
 
     void MoveTo(Vector2 targetPos)
     {
         var distanceToTarget = Vector2.Distance(targetPos, head.transform.position);
 
-        nextDir.x = (targetPos.x - head.transform.position.x) / distanceToTarget;
-        nextDir.y = (targetPos.y - head.transform.position.y) / distanceToTarget;
+        headDirection.x = (targetPos.x - head.transform.position.x) / distanceToTarget;
+        headDirection.y = (targetPos.y - head.transform.position.y) / distanceToTarget;
+
+        rotateHead();
+    }
+
+    private void rotateHead()
+    {
+        var radian = Mathf.Atan2(headDirection.y, headDirection.x);
+        var degree = Mathf.Rad2Deg * radian;
+
+        head.transform.rotation = Quaternion.Euler(0, 0, degree);
     }
 }
